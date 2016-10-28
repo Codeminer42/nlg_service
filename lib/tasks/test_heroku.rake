931b0b40837cefffe09d6d9e9f1bde66a1196dff
Fabio Akita <fabioakita@gmail.com>
2016-10-28 09:32:14 -0200

    Adding a more complex test and benchmark support for the rake task for real testing

--- lib/tasks/test_heroku.rake
+++ lib/tasks/test_heroku.rake
@@ -2,16 +2,23 @@ desc "Test the Heroku endpoint"
 task test_heroku: :environment do
   require "net/http"
   require "uri"
+  require "benchmark"
 
   domain = ENV['HEROKU'] || "nlg-service.herokuapp.com"
   uri = URI.parse("http://#{domain}/api/realiser")
 
-  xml_test = File.read(Rails.root.join("test/fixtures/appositive_payload.xml"))
-  response = Net::HTTP.post_form(uri, {"xml" => xml_test})
+  xml_test = File.read(Rails.root.join("test/fixtures/complex_test_payload.xml"))
 
-  puts "Sending this XML payload:"
-  puts xml_test
+  Benchmark.bm do |bm|
+    bm.report do
+      response = Net::HTTP.post_form(uri, {"xml" => xml_test})
 
-  puts "Received this NLG string:"
-  puts response.body
+      puts "Sending this XML payload:"
+      puts xml_test
+
+      puts "Received this NLG string:"
+      puts response.body
+    end
+  end
+
 end
--- test/controllers/api/realisers_controller_test.rb
+++ test/controllers/api/realisers_controller_test.rb
@@ -5,6 +5,7 @@ class Api::RealisersControllerTest < ActionController::TestCase
   setup do
     @appositive_payload = File.read(Rails.root.join("test/fixtures/appositive_payload.xml"))
     @a_problem_payload = File.read(Rails.root.join("test/fixtures/a_problem_payload.xml"))
+    @complex_payload = File.read(Rails.root.join("test/fixtures/complex_test_payload.xml"))
   end
 
   test "realise appositive recording" do
@@ -17,6 +18,40 @@ class Api::RealisersControllerTest < ActionController::TestCase
     post :create, xml: @a_problem_payload
     assert_response :success
     assert_equal assigns[:realisation], "There is an eccentric, tubular stenosis in the third obtuse marginal branch.There is an 80 % stenosis in the proximal right coronary artery."
+  end
+
+  test "realise a complex recording with multiple records" do
+    post :create, xml: @complex_payload
+    assert_response :success
+    assert_equal assigns[:realisation], "Hello, world.
+
+
+The patient lied etherised upon a table. The patient lied etherised upon a table.
+
+
+* an 18 thing but a 180 thing
+* an 18x thing but a 08 thing
+* an 11g thing but a 9i thing
+* an 8th thing but a 9th thing
+* an 11th thing but a 1100 thing
+* an 11.000 thing but an 11000 thing
+* an 81000 thing but a 180,000 thing
+* an 81,000 thing but a 01834 thing
+* an 8% thing but a 9% thing
+* an 8432425 thing but a 42nd thing
+
+There is an 80 % in-stent restenosis in the proximal right coronary artery.
+
+
+There is a 95 %, eccentric in-stent restenosis (8 mm length) in the proximal right coronary artery.
+
+
+The right coronary artery is a vessel with luminal irregularities.
+
+
+* Normal coronary arteries.
+* Normal left heart hemodynamics.
+* Normal right heart hemodynamics."
   end
 
 end
